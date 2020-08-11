3320f67cf737b463c240fcb87d0219029ac03e1a
COAB1 <carol.o.ag@gmail.com>
2020-08-11 16:08:07 -0300

    Create the model with 'rails generate model'

--- spec/features/admin_view_car_categories_spec.rb
+++ spec/features/admin_view_car_categories_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 feature 'Admin view car categories' do
   scenario 'successfully' do
+  # Arrange -> preparação dos dados
     CarCategory.create!(name: 'Top', daily_rate: 105.5, car_insurance: 58.5,
                         third_party_insurance: 10.5)
     CarCategory.create!(name: 'Flex', daily_rate: 80, car_insurance: 8.5,
                         third_party_insurance: 8.5)
-
+  # Act -> Executra o código
     visit root_path
     click_on 'Categorias'
 
