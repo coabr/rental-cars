df751f607ab91cced346ad4de77e30664fc88af5
COAB1 <carol.o.ag@gmail.com>
2020-08-16 16:36:05 -0300

    Refactoring the code

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -12,8 +12,14 @@ class CarCategoriesController < ApplicationController
   end
 
   def create
-    car_category_params = params.require(:car_category).permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
     @car_category = CarCategory.create(car_category_params) # o .create faz um .new e um .save
-    redirect_to car_category_path(id: @car_category.id) #toda ação que nao é um GET voce tem que direcionar para algum lugar
+    redirect_to (@car_category) #toda ação que nao é um GET voce tem que direcionar para algum lugar // nao precisa do id, porque o ruby preenche automaticamente quando temos um objeto como parametro // nao precisa da rota porque o ruby entende que se quero mostrar um objeto vou ter que redirecionar para o path de #show desse object
+  end
+
+  private # tudo que está daqui pra baixo não é uma action, é um método
+
+  def car_category_params
+    params.require(:car_category)
+          .permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
   end
 end
