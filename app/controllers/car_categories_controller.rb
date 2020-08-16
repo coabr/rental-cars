7860b2f4cf9d5099fd53909147c37ee30e925f22
COAB1 <carol.o.ag@gmail.com>
2020-08-16 15:15:37 -0300

    Solve the 'Admin register car categories' test

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -6,4 +6,14 @@ class CarCategoriesController < ApplicationController
   def show
     @car_category = CarCategory.find(params[:id]) #params é um HASH {chave => valor}
   end
+
+  def new
+    @car_category = CarCategory.new
+  end
+
+  def create
+    car_category_params = params.require(:car_category).permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
+    @car_category = CarCategory.create(car_category_params) # o .create faz um .new e um .save
+    redirect_to car_category_path(id: @car_category.id) #toda ação que nao é um GET voce tem que direcionar para algum lugar
+  end
 end
