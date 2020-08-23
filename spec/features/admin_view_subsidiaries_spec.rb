ce974d338ee49b0f51633c8c62dfe8c8f0436ca3
COAB1 <carol.o.ag@gmail.com>
2020-08-23 18:14:57 -0300

    Create manually for the first time a test called 'Admin view subsidiaries'

---
+++ spec/features/admin_view_subsidiaries_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Admin view subsidiaries' do
+	scenario 'successfully' do
+		# Arrange
+		Subsidiary.create!(name: 'Fiori', cnpj: '53.306.981/0001-66' , address: 'Estrada do encanamento, 1752') 
+		Subsidiary.create!(name: 'Localiza', cnpj: '23.791.285/0001-21' , address: 'Estrada do arraial, 1777') 
+		
+
+		#Act
+		visit root_path
+		click_on 'FIliais'
+		
+		#Assert
+		expect(page).to have_content('FIori')
+		expect(page).to have_content('Estrada do encanamento, 1752')
+		expect(page).to have_content('Localiza')
+		expect(page).to have_content('Estrada do arraial, 1777')
+	end
+end
